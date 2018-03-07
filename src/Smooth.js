9f23b1e43f6d822442f4b864920b49a66181de8e
Frederic Heem <frederic.heem@gmail.com>
2018-03-07 15:48:04 +0000

    remove keyframe css

--- src/Smooth.js
+++ src/Smooth.js
@@ -1,61 +1,79 @@
 import React from "react";
 
-function createKeyframeTag(css, id) {
+function createKeyFrameTag(css, name) {
   const elem = document.createElement("style");
   elem.innerHTML = css;
   elem.setAttribute("class", "keyframe-style");
-  elem.setAttribute("id", id);
+  elem.setAttribute("id", name);
   elem.setAttribute("type", "text/css");
   document.getElementsByTagName("head")[0].appendChild(elem);
-  return id;
+  return elem;
 }
 
-export default class Morph extends React.Component {
+function createKeyFrameCss(name, old, next) {
+  return `@keyframes ${name} {
+      from { 
+          opacity: 1;
+          height:${old.height}px;
+          width:${old.width}px;
+      }
+      to { 
+          opacity: 1;
+          height:${next.height}px;
+          width:${next.width}px;
+      }
+    }`;
+}
 
+function createKeyFrameName() {
+  const id = Math.random()
+    .toString(36)
+    .substring(7);
+  return `kf-${id}`;
+}
+
+function removeCssElement(elem) {
+  if (elem) {
+    elem.parentNode.removeChild(elem);
+  }
+}
+
+export default class Smooth extends React.Component {
   componentDidMount() {
     this.nodeCurrentRect = this.nodeCurrent.getBoundingClientRect();
-    console.log("componentDidMount ", this.nodeCurrentRect);
   }
   componentWillUnmount() {
-    //TODO remove css
+    removeCssElement(this.cssElem);
   }
 
   render() {
     const { state } = this;
     const me = this;
     return (
-      <div {...this.props} style={{overflow: 'hidden'}}
+      <div
+        {...this.props}
+        style={{ overflow: "hidden" }}
         ref={node => {
           if (node && node.innerHTML) {
             if (this.nodeCurrentRect) {
               const old = this.nodeCurrentRect;
               const next = node.getBoundingClientRect();
-              const id = Math.random()
-                .toString(36)
-                .substring(7);
-              const name = `kf-${id}`;
-              const keyFrame = `@keyframes ${name} {
-                                  from { 
-                                      opacity: 1;
-                                      height:${old.height}px;
-                                      width:${old.width}px;
-                                  }
-                                  to { 
-                                      opacity: 1;
-                                      height:${next.height}px;
-                                      width:${next.width}px;
-                                  }
-                                }`;
-              createKeyframeTag(keyFrame, id);
+              const name = createKeyFrameName();
+              const keyFrame = createKeyFrameCss(name, old, next);
+              const cssElement = createKeyFrameTag(keyFrame, name);
+              removeCssElement(this.cssElem);
+              this.cssElem = cssElement;
               node.style.animation = `${name} 0.3s`;
+              const animationBeginHandler = () => {
+                node.removeEventListener("animationend", animationBeginHandler);
+                this.nodeCurrentRect = node.getBoundingClientRect();
+                removeCssElement(me.cssElem);
+                me.cssElem = null;
+              };
+
+              node.addEventListener("animationend", animationBeginHandler);
             }
 
-            const animationBeginHandler = () => {
-              node.removeEventListener("animationend", animationBeginHandler);
-              this.nodeCurrentRect = node.getBoundingClientRect();
-            };
-
-            node.addEventListener("animationend", animationBeginHandler);
             me.nodeCurrent = node;
           }
         }}
